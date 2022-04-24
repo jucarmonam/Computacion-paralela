87e37501232e428f80c889bffb1c1e3939bd554a
juarodriguezc <juarodriguezc@unal.edu.co>
2022-04-24 00:26:19 -0500

    First version working :)

--- practica1/image-effect.c
+++ practica1/image-effect.c
@@ -16,7 +16,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
-#define R_ARGS 3
+#define R_ARGS 4
 #define EXPORT_QUALITY 100
 
 void initializeMatrix(int *matR, int *matG, int *matB, int width, int height, int channels, unsigned char *img)
@@ -33,7 +33,7 @@ void initializeMatrix(int *matR, int *matG, int *matB, int width, int height, in
     }
 }
 
-void createImage(int *matR, int *matG, int *matB, int width, int height, int channels, unsigned char *resImg, unsigned char *img)
+void joinMatrix(int *matR, int *matG, int *matB, int width, int height, int channels, unsigned char *resImg, unsigned char *img)
 {
     int i = 0, j = 0;
     for (i = 0; i < height; i++)
@@ -52,10 +52,11 @@ void createImage(int *matR, int *matG, int *matB, int width, int height, int cha
 int main(int argc, char *argv[])
 {
     /*Declarar los string de lectura y escritura*/
-    char *loadPath;
-    char *savePath;
+    char *loadPath, *savePath;
     /*Variable para yo que se xd*/
     int arg = 0;
+    /*Variable para el número de hilos*/
+    int nThreads = 0;
     /*Declaración de variable para la escritura del archivo*/
     FILE *fp;
     /*Declarar las variables necesarias para leer la imagen*/
@@ -64,17 +65,28 @@ int main(int argc, char *argv[])
     unsigned char *img, *resImg;
     /*Crear las tres matrices para cada canal de color*/
     int *matR, *matG, *matB;
+    /*Crear las tres matrices resultanres*/
+    int *rMatR, *rMatG, *rMatB;
+    /**/
+    int kernel[] = {-1,0,1,-2,0,2,-1,0,1};
     /*Verificar que la cantidad de argumentos sea la correcta*/
     if ((argc - 1) < R_ARGS)
     {
-        printf("Son necesarios 3 argumentos para el funcionamiento\n");
-        printf("Para una correcta ejecución: ./main input_image output_image parameter\n");
+        printf("Son necesarios %d argumentos para el funcionamiento\n", R_ARGS);
+        printf("Para una correcta ejecución: ./main input_image output_image parameter threads\n");
         exit(1);
     }
     /*Cargar en las variables los parametros*/
     loadPath = *(argv + 1);
     savePath = *(argv + 2);
     arg = atoi(*(argv + 3));
+    nThreads = atoi(*(argv + 4));
+    /*Verificar que el número de hilos sea válido*/
+    if (nThreads < 0)
+    {
+        printf("El número de hilos ingresado no es válido \n");
+        exit(1);
+    }
     /*Cargar la imagen usando el parámetro con el nombre*/
     img = stbi_load(loadPath, &width, &height, &channels, 0);
     /*Verificar que la imagen exista y sea cargada correctamente*/
@@ -95,7 +107,91 @@ int main(int argc, char *argv[])
     /*Inicializar las matrices con los valores de la imagen*/
     initializeMatrix(matR, matG, matB, width, height, channels, img);
     /*Logica del filtro*/
-    
+    /*Crear las matrices de Color con para los resultados*/
+    rMatR = (int *)malloc(height * width * sizeof(int));
+    rMatG = (int *)malloc(height * width * sizeof(int));
+    rMatB = (int *)malloc(height * width * sizeof(int));
+
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+        {
+            float prom = (*(matR + (i * width + j)) + *(matG + (i * width + j)) + *(matB + (i * width + j)))/3.0;
+            *(matR + (i * width + j)) = (int)prom;
+            *(matG + (i * width + j)) = (int)prom;
+            *(matB + (i * width + j)) = (int)prom;
+
+
+
+
+            /*
+            *(rMatR + (i * width + j)) = (*(kernel) * *(matR + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matR + ((i - 1) * width + j)) + *(kernel + 2) * *(matR + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matR + (i  * width + j - 1)) + *(kernel+4) * *(matR + (i * width + j)) + *(kernel+ 5) * *(matR + (i * width + j + 1))
+                                        + *(kernel+6) * *(matR + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matR + ((i + 1) * width + j)) + *(kernel+8) * *(matR + ((i + 1) * width + j + 1)))%255;
+
+            /*Convolucion para el canal G*/
+
+            /*
+            *(rMatG + (i * width + j)) = (*(kernel) * *(matG + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matG + ((i - 1) * width + j)) + *(kernel + 2) * *(matG + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matG + (i  * width + j - 1)) + *(kernel+4) * *(matG + (i * width + j)) + *(kernel+ 5) * *(matG + (i * width + j + 1))
+                                        + *(kernel+6) * *(matG + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matG + ((i + 1) * width + j)) + *(kernel+8) * *(matG + ((i + 1) * width + j + 1)))%255;
+            */
+            /*Convolucion para el canal B*/
+
+            //*(rMatB + (i * width + j)) = 0;
+
+            /*
+            *(rMatB + (i * width + j)) = (*(kernel) * *(matB + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matB + ((i - 1) * width + j)) + *(kernel + 2) * *(matB + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matB + (i  * width + j - 1)) + *(kernel+4) * *(matB + (i * width + j)) + *(kernel+ 5) * *(matB + (i * width + j + 1))
+                                        + *(kernel+6) * *(matB + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matB + ((i + 1) * width + j)) + *(kernel+8) * *(matB + ((i + 1) * width + j + 1)))%255;
+            */
+
+        }
+    }
+
+    for (i = 1; i < height-1; i++)
+    {
+        for (j = 1; j < width-1; j++)
+        {
+
+
+
+            int conv = (*(kernel) * *(matR + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matR + ((i - 1) * width + j)) + *(kernel + 2) * *(matR + ((i - 1) * width + j + 1))
+                        + *(kernel+3) * *(matR + (i  * width + j - 1)) + *(kernel+4) * *(matR + (i * width + j)) + *(kernel+ 5) * *(matR + (i * width + j + 1))
+                        + *(kernel+6) * *(matR + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matR + ((i + 1) * width + j)) + *(kernel+8) * *(matR + ((i + 1) * width + j + 1)));
+            
+            *(rMatR + (i * width + j)) = conv < 0 ? 0 : conv;
+            *(rMatG + (i * width + j)) = conv < 0 ? 0 : conv;
+            *(rMatB + (i * width + j)) = conv < 0 ? 0 : conv;
+
+            /*
+            *(rMatR + (i * width + j)) = (*(kernel) * *(matR + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matR + ((i - 1) * width + j)) + *(kernel + 2) * *(matR + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matR + (i  * width + j - 1)) + *(kernel+4) * *(matR + (i * width + j)) + *(kernel+ 5) * *(matR + (i * width + j + 1))
+                                        + *(kernel+6) * *(matR + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matR + ((i + 1) * width + j)) + *(kernel+8) * *(matR + ((i + 1) * width + j + 1)))%255;
+
+            /*Convolucion para el canal G*/
+
+            /*
+            *(rMatG + (i * width + j)) = (*(kernel) * *(matG + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matG + ((i - 1) * width + j)) + *(kernel + 2) * *(matG + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matG + (i  * width + j - 1)) + *(kernel+4) * *(matG + (i * width + j)) + *(kernel+ 5) * *(matG + (i * width + j + 1))
+                                        + *(kernel+6) * *(matG + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matG + ((i + 1) * width + j)) + *(kernel+8) * *(matG + ((i + 1) * width + j + 1)))%255;
+            */
+            /*Convolucion para el canal B*/
+
+            //*(rMatB + (i * width + j)) = 0;
+
+            /*
+            *(rMatB + (i * width + j)) = (*(kernel) * *(matB + ((i - 1) * width + j - 1)) + *(kernel+1) * *(matB + ((i - 1) * width + j)) + *(kernel + 2) * *(matB + ((i - 1) * width + j + 1))
+                                        + *(kernel+3) * *(matB + (i  * width + j - 1)) + *(kernel+4) * *(matB + (i * width + j)) + *(kernel+ 5) * *(matB + (i * width + j + 1))
+                                        + *(kernel+6) * *(matB + ((i + 1) * width + j - 1)) + *(kernel+7) * *(matB + ((i + 1) * width + j)) + *(kernel+8) * *(matB + ((i + 1) * width + j + 1)))%255;
+            */
+
+        }
+    }
+
     /*Exportar la imagen resultante*/
     /*Reservar el espacio de memoria para la imagen resultante*/
     resImg = malloc(width * height * channels);
@@ -104,7 +200,7 @@ int main(int argc, char *argv[])
         printf("Error al crear la imagen, problema con malloc \n");
         exit(1);
     }
-    createImage(matR, matG, matB, width, height, channels, resImg, img);
+    joinMatrix(rMatR, rMatG, rMatB, width, height, channels, resImg, img);
     /*Guardar la imagen con el nombre especificado*/
     if (strstr(savePath, ".png"))
         stbi_write_png(savePath, width, height, channels, resImg, width * channels);
@@ -114,6 +210,9 @@ int main(int argc, char *argv[])
     free(matR);
     free(matG);
     free(matB);
+    free(rMatR);
+    free(rMatG);
+    free(rMatB);
     free(resImg);
     stbi_image_free(img);
     return 0;
---
+++ practica1/script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Computación paralela y distribuida - práctica 1"
+echo "Compilando el programa ..."
+gcc image-effect.c -o my-effect -lm
+sudo ./my-effect ./img/1080p/img_1.jpg img_res.jpg 10 5
